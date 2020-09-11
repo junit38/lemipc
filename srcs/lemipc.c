c371b6cded0af241aa29b60bd70929190c51461d
Junit38 <Junit38@gmail.com>
2020-09-11 17:13:56 +0200

    norm

--- srcs/lemipc.c
+++ srcs/lemipc.c
@@ -6,7 +6,7 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/09/11 17:11:58 by mery             ###   ########.fr       */
+/*   Updated: 2020/09/11 17:13:51 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,14 @@ static int	get_shm_id_data(void)
 	return (shm_id);
 }
 
-static int 	get_shm_id_map(void)
+static int	get_shm_id_map(void)
 {
-	int 	shm_id;
+	int		shm_id;
 	key_t	shm_key;
 
 	shm_key = ftok("map.txt", 'x');
-	shm_id = shmget(shm_key, sizeof(struct s_player) * (MAPSIZE * MAPSIZE), IPC_CREAT | 0666);
+	shm_id = shmget(shm_key,
+		sizeof(struct s_player) * (MAPSIZE * MAPSIZE), IPC_CREAT | 0666);
 	if (shm_id < 0)
 	{
 		printf("shmget error\n");
@@ -42,7 +43,7 @@ static int 	get_shm_id_map(void)
 	return (shm_id);
 }
 
-static int 	get_msq_id(void)
+static int	get_msq_id(void)
 {
 	int 	msq_id;
 	key_t	shm_key;
@@ -70,18 +71,13 @@ t_data		*init_data(t_data *data)
 
 int			main(int argc, char **argv)
 {
-	int 	shm_id;
+	int		shm_id;
 
 	if (argc == 2)
 	{
 		initsignal();
 		shm_id = get_shm_id_data();
 		g_data = (struct s_data *)shmat(shm_id, NULL, 0);
-		if (ft_strcmp(argv[1], "clean") == 0)
-		{
-			clean_game(g_data);
-			exit(1);
-		}
 		if (g_data->filled != 1)
 		{
 			g_data->data_id = shm_id;
