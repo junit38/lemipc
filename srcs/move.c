3b616bc1866921ab1a5ecf60a948e18367e89450
Junit38 <Junit38@gmail.com>
2020-09-23 16:12:02 +0200

    norm

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/09/23 16:10:51 by mery             ###   ########.fr       */
+/*   Updated: 2020/09/23 16:12:00 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,29 +33,29 @@ static t_player	*get_next_move(t_data *data, t_player *player, int d_x, int d_y)
 	return (NULL);
 }
 
-static t_player	*get_next_move_2(t_data *data, t_player *player, t_player *to_move)
+static t_player	*get_next_move_2(t_data *data, t_player *plyer, t_player *to_mv)
 {
 	int		d_x;
 	int		d_y;
 
-	d_x = to_move->x - player->x;
-	d_y = to_move->y - player->y;
+	d_x = to_mv->x - plyer->x;
+	d_y = to_mv->y - plyer->y;
 	if (d_x == 0 && d_y < 0)
-		return (get_case(data, player->x + 1, player->y - 1));
+		return (get_case(data, plyer->x + 1, plyer->y - 1));
 	else if (d_x > 0 && d_y < 0)
-		return (get_case(data, player->x + 1, player->y));
+		return (get_case(data, plyer->x + 1, plyer->y));
 	else if (d_x > 0 && d_y == 0)
-		return (get_case(data, player->x + 1, player->y + 1));
+		return (get_case(data, plyer->x + 1, plyer->y + 1));
 	else if (d_x > 0 && d_y > 0)
-		return (get_case(data, player->x, player->y + 1));
+		return (get_case(data, plyer->x, plyer->y + 1));
 	else if (d_x == 0 && d_y > 0)
-		return (get_case(data, player->x - 1, player->y + 1));
+		return (get_case(data, plyer->x - 1, plyer->y + 1));
 	else if (d_x < 0 && d_y > 0)
-		return (get_case(data, player->x - 1, player->y));
+		return (get_case(data, plyer->x - 1, plyer->y));
 	else if (d_x < 0 && d_y == 0)
-		return (get_case(data, player->x - 1, player->y - 1));
+		return (get_case(data, plyer->x - 1, plyer->y - 1));
 	else if (d_x < 0 && d_y < 0)
-		return (get_case(data, player->x, player->y - 1));
+		return (get_case(data, plyer->x, plyer->y - 1));
 	return (NULL);
 }
 
@@ -70,7 +70,7 @@ static void		move_to(t_player *player, t_player *to_move)
 	player->cible_y = -1;
 }
 
-t_player	*player_move(t_data *data, t_player *player)
+t_player		*player_move(t_data *data, t_player *player)
 {
 	int			diff_x;
 	int			diff_y;
@@ -83,8 +83,7 @@ t_player	*player_move(t_data *data, t_player *player)
 		diff_x = player->cible_x - player->x;
 		diff_y = player->cible_y - player->y;
 		to_move = get_next_move(data, player, diff_x, diff_y);
-		if (to_move && to_move->is_player == 1
-			&& to_move->team == player->team)
+		if (to_move && to_move->is_player == 1 && to_move->team == player->team)
 			to_move = get_next_move_2(data, player, to_move);
 		if (to_move && to_move->is_player == 0)
 		{
